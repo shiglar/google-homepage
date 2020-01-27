ba615e2564d494e19bebd15047c88c089768501a
adam shigley <adamshigley@ymail.com>
2020-01-26 18:22:35 -0700

    Added Google Search page clone directory and more styling

---
+++ search.js
@@ -0,0 +1,13 @@
+hello = () => {
+    alert("hi");
+    window.location.replace("www.google.com");
+}
+
+lucky = () => {
+    var x = document.getElementsByClassName("feeling-lucky")[0];
+    if (x.value != "I'm Feeling Happy") {
+        x.setAttribute("value", "I'm Feeling Happy")
+    } else {
+        x.setAttribute("value", "I'm Feeling Lucky")
+    }
+}
